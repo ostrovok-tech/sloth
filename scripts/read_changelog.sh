0b27dc316059f7ef0acb87d82c1d9ae5ff379cc2
nixargh <nixargh@protonmail.com>
2024-09-10 17:06:54 +0300

    lint scripts/read_changelog.sh

--- scripts/read_changelog.sh
+++ scripts/read_changelog.sh
@@ -9,34 +9,32 @@ TAG=$1
 
 stringContain() {
 	case $1 in
-	       	$2*)
-		       	return 0
-			;;
-		*)
-		       	return 1
-			;;
+	$2*)
+		return 0
+		;;
+	*)
+		return 1
+		;;
 	esac
 }
 
 while IFS= read -r LINE; do
 	case "$FOUND" in
-		0)
+	0)
 		if stringContain "${LINE}" "## [${TAG}"; then
-			echo $LINE
+			echo "${LINE}"
 			FOUND=$((FOUND + 1))
 		fi
 		;;
-
-		1)
-			if stringContain "${LINE}" "## ["; then
-				FOUND=$((FOUND + 1))
-			else
-				echo $LINE
-			fi
-			;;
-
-		2)
-			break
-			;;
+	1)
+		if stringContain "${LINE}" "## ["; then
+			FOUND=$((FOUND + 1))
+		else
+			echo "${LINE}"
+		fi
+		;;
+	2)
+		break
+		;;
 	esac
-done < $CHANGELOG
+done <$CHANGELOG
